0521ef701cea92801e5382d7d44ad6f9452fd8ee
Damien CHABRET <damien.chabret@iut-dijon.u-bourgogne.fr>
2024-02-25 23:56:13 +0100

    amelioration projectDetails

--- src/client/models.py
+++ src/client/models.py
@@ -6,3 +6,7 @@ Project model
 class Project(models.Model):
     project_name = models.CharField(max_length=30)
     project_description = models.CharField(max_length=300)
+    usedLangages = models.CharField(max_length=100)
+    usedTools = models.CharField(max_length=100, blank = True)
+    usedSkills = models.CharField(max_length=100, blank = True)
+    github = models.CharField(max_length=100, blank = True)
--- src/client/views.py
+++ src/client/views.py
@@ -19,6 +19,19 @@ display detail of the project
 def projectDetail(request, project_name):
     try:
         project = Project.objects.get(project_name=project_name)
+        usedSkills = project.usedSkills.split(";")
+        usedTools = project.usedTools.split(";")
+
+
     except Project.DoesNotExist:
         raise Http404("Project does not exist")
-    return render(request, 'projects/projectDetail.html', {"project": project})
+    
+    return render(
+        request, 'projects/projectDetail.html', 
+        {
+            "project": project, 
+            "usedLangages":project.usedLangages.split(";"),
+            "usedSkills": usedSkills,
+            "usedTools": usedTools
+        }
+    )
