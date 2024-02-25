102d6d475f7d2b9782e5de559784ac2ba8849def
Damien CHABRET <damien.chabret@iut-dijon.u-bourgogne.fr>
2024-02-26 00:05:12 +0100

    doc changement

--- src/client/models.py
+++ src/client/models.py
@@ -1,12 +1,43 @@
 from django.db import models
 
-"""
-Project model
-"""
 class Project(models.Model):
+    """
+    Project model
+    """
+
     project_name = models.CharField(max_length=30)
     project_description = models.CharField(max_length=300)
     usedLangages = models.CharField(max_length=100)
     usedTools = models.CharField(max_length=100, blank = True)
     usedSkills = models.CharField(max_length=100, blank = True)
     github = models.CharField(max_length=100, blank = True)
+
+    def get_used_skills_list(self):
+        """
+        Get all the skills
+        """
+
+        tab = []
+        if self.usedSkills:
+            tab = self.usedSkills.split(";")
+        return tab
+
+    def get_used_langages_list(self):
+        """
+        Get all the langages 
+        """
+
+        tab = []
+        if self.usedLangages:
+            tab = self.usedLangages.split(";")
+        return tab
+
+    def get_used_tools_list(self):
+        """
+        get all the tools 
+        """
+
+        tab = []
+        if self.usedTools:
+            tab = self.usedTools.split(";")
+        return tab
--- src/client/views.py
+++ src/client/views.py
@@ -3,25 +3,25 @@ from django.shortcuts import render
 
 from .models import Project
 
-"""
-Display all the projects
-"""
 def projects(request):
+    """
+    Display all the projects
+    """
+
     projects = Project.objects.all()
     context = {
         "projects": projects
     }
     return render(request, "projects/projects.html", context)
 
-"""
-display detail of the project
-"""
+
 def projectDetail(request, project_name):
+    """
+    display detail of the project
+    """
+
     try:
         project = Project.objects.get(project_name=project_name)
-        usedSkills = project.usedSkills.split(";")
-        usedTools = project.usedTools.split(";")
-
 
     except Project.DoesNotExist:
         raise Http404("Project does not exist")
@@ -29,9 +29,6 @@ def projectDetail(request, project_name):
     return render(
         request, 'projects/projectDetail.html', 
         {
-            "project": project, 
-            "usedLangages":project.usedLangages.split(";"),
-            "usedSkills": usedSkills,
-            "usedTools": usedTools
+            "project": project,
         }
     )
