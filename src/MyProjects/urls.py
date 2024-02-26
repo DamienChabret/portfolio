6a18fd56dda30d82d9c9c047bf8b2ff6191364e6
Damien CHABRET <damien.chabret@iut-dijon.u-bourgogne.fr>
2024-02-26 19:27:49 +0100

    depression à cause de bug pour link css ....

--- src/MyProjects/urls.py
+++ src/MyProjects/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.conf.urls.static import static
+from MyProjects import settings
 
 urlpatterns = [
     path("", include("client.urls")),
     path('admin/', admin.site.urls),
-]
+] 
--- src/client/urls.py
+++ src/client/urls.py
@@ -1,7 +1,10 @@
+from django.conf.urls.static import static
 from django.urls import path
+
+from MyProjects import settings
 from . import views;
 
 urlpatterns = [
     path("", views.projects, name="Home"),
-    path("<str:project_name>/", views.projectDetail, name="detail")
+    path("<str:project_name>/", views.projectDetail, name="detail"),
 ]
