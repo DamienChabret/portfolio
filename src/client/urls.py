c4eaaba69fe8268b0c74886b40b9029ac0dbe73b
Damien CHABRET <damien.chabret@iut-dijon.u-bourgogne.fr>
2024-02-28 01:42:21 +0100

    début design pages projets

--- src/client/urls.py
+++ src/client/urls.py
@@ -3,7 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path("", views.index, name="home"),
-    path("projects", views.projects, name="projects"),
+    path("", views.projects, name="projects"),
     path("<str:project_name>/", views.projectDetail, name="detail"),
 ]
